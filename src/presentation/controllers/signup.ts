910ba5f98b6e64e768612e07d2e346503ae4cf3a
mades eladrian <mades.eladrian@gmail.com>
2022-06-17 12:01:53 -0300

    refactor: remove code duplication in signup

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -12,18 +12,18 @@ export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      const { email, password, passwordConfirmation } = httpRequest.body
+      const isValid = this.emailValidator.isValid(email)
 
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
-
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
