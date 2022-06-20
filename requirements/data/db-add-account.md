a26aeb218fb422138039016bc6fcdc3bebd9aaa7
mades eladrian <mades.eladrian@gmail.com>
2022-06-20 10:40:06 -0300

    test: ensure DbAddAccount returns an account on success

--- requirements/data/db-add-account.md
+++ requirements/data/db-add-account.md
@@ -4,3 +4,4 @@
 2. ✅ **Retorna** um erro se o Encrypter falhar
 3. ✅ **Chama** o AddAccountRepository com os valores corretos
 4. ✅ **Retorna** um erro se o AddAccountRepository falhar
+5. ✅ **Retorna** um Account em caso de sucesso
--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -99,4 +99,20 @@ describe('DbAddAccount Use Case', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('5 - Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    await expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
+    })
+  })
 })
