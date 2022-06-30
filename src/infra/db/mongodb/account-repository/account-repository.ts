237a1f0baa529277302a93a2ca263164c0f47138
mades eladrian <mades.eladrian@gmail.com>
2022-06-30 13:54:08 -0300

    feat: ensure AccountRepository returns an nnull on loadByEmail fails

--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -1,13 +1,22 @@
 import { AccountModel } from '../../../../domain/models'
 import { AddAccountModel } from '../../../../domain/usecases'
-import { AddAccountRepository } from '../../../../data/protocols/db'
+import {
+  AddAccountRepository,
+  LoadAccountByEmailRepository
+} from '../../../../data/protocols/db'
 import { MongoHelper } from '../helpers'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
     return MongoHelper.map(account)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.map(account)
   }
 }
