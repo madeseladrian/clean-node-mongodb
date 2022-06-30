a4a05214a67343308b1ddeda38f910543b8c3941
mades eladrian <mades.eladrian@gmail.com>
2022-06-30 16:40:00 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,11 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -13,7 +17,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -26,6 +30,24 @@ describe('Login Routes', () => {
           email: 'mades@gmail.com',
           password: '123456',
           passwordConfirmation: '123456'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('1 - Should return 200 on signup', async () => {
+      const password = await hash('123456', 12)
+      await accountCollection.insertOne({
+        name: 'Mades',
+        email: 'mades@gmail.com',
+        password: password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'mades@gmail.com',
+          password: '123456'
         })
         .expect(200)
     })
