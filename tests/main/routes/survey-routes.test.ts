f62fdbe723ae595a85a53f9c87e5f88580500f9e
mades eladrian <mades.eladrian@gmail.com>
2022-07-10 21:59:12 -0300

    test: ensure add survey route returns 204 on add survey with valid accessToken

--- tests/main/routes/survey-routes.test.ts
+++ tests/main/routes/survey-routes.test.ts
@@ -1,41 +1,84 @@
 import { MongoHelper } from '@/infra/db'
 import { setupApp } from '@/main/config/app'
+import env from '@/main/config/env'
 import { Express } from 'express'
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
-let surveyCollection: Collection
-let app: Express
-
 describe('Surveys Routes', () => {
-  beforeAll(async () => {
-    app = await setupApp()
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
+  let surveyCollection: Collection
+  let accountCollection: Collection
+  let app: Express
 
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
+  const mockAccessToken = async (): Promise<string> => {
+    const res = await accountCollection.insertOne({
+      name: 'Mades',
+      email: 'mades@gmail.com',
+      password: '123456',
+      role: 'admin'
+    })
+    const id = res.insertedId.toHexString()
+    const accessToken = sign({ id }, env.jwtSecret)
+    await accountCollection.updateOne({
+      _id: res.insertedId
+    }, {
+      $set: {
+        accessToken
+      }
+    })
+    return accessToken
+  }
 
-  beforeEach(async () => {
-    surveyCollection = MongoHelper.getCollection('surveys')
-    await surveyCollection.deleteMany({})
-  })
+  describe('Survey Routes', () => {
+    beforeAll(async () => {
+      app = await setupApp()
+      await MongoHelper.connect(process.env.MONGO_URL)
+    })
 
-  describe('POST /surveys', () => {
-    test('1 - Should return 403 on add survey without accessToken', async () => {
-      await request(app)
-        .post('/api/surveys')
-        .send({
-          question: 'Question',
-          answers: [{
-            answer: 'Answer 1',
-            image: 'http://image-name.com'
-          }, {
-            answer: 'Answer 2'
-          }]
-        })
-        .expect(403)
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+      surveyCollection = MongoHelper.getCollection('surveys')
+      await surveyCollection.deleteMany({})
+      accountCollection = MongoHelper.getCollection('accounts')
+      await accountCollection.deleteMany({})
+    })
+
+    describe('POST /surveys', () => {
+      test('1 - Should return 403 on add survey without accessToken', async () => {
+        await request(app)
+          .post('/api/surveys')
+          .send({
+            question: 'Question',
+            answers: [{
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            }, {
+              answer: 'Answer 2'
+            }]
+          })
+          .expect(403)
+      })
+
+      test('2 - Should return 204 on add survey with valid accessToken', async () => {
+        const accessToken = await mockAccessToken()
+        await request(app)
+          .post('/api/surveys')
+          .set('x-access-token', accessToken)
+          .send({
+            question: 'Question',
+            answers: [{
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            }, {
+              answer: 'Answer 2'
+            }]
+          })
+          .expect(204)
+      })
     })
   })
 })
