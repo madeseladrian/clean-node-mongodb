ee84a7d1d46ab82c03c053c27e015452f8e95854
mades eladrian <mades.eladrian@gmail.com>
2022-07-10 21:09:20 -0300

    feat: ensure JwtAdapter calls verify with correct values

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -7,4 +7,8 @@ export class JwtAdapter implements Encrypter {
   async encrypt (plaintext: string): Promise<string> {
     return jwt.sign({ id: plaintext }, this.secret)
   }
+
+  async decrypt (ciphertext: string): Promise<string> {
+    return jwt.verify(ciphertext, this.secret) as any
+  }
 }
