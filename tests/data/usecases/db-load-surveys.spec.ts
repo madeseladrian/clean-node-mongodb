f31c55f34e5af711a89c499c4035649755bd16c7
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 10:58:10 -0300

    test: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ tests/data/usecases/db-load-surveys.spec.ts
@@ -0,0 +1,35 @@
+import { DbLoadSurveys } from '@/data/usecases'
+import { LoadSurveysRepositorySpy } from '@/tests/data/mocks'
+import faker from 'faker'
+import MockDate from 'mockdate'
+
+type SutTypes = {
+  sut: DbLoadSurveys
+  loadSurveysRepositorySpy: LoadSurveysRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysRepositorySpy = new LoadSurveysRepositorySpy()
+  const sut = new DbLoadSurveys(loadSurveysRepositorySpy)
+  return {
+    sut,
+    loadSurveysRepositorySpy
+  }
+}
+
+describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('1 - Should call LoadSurveysRepository', async () => {
+    const { sut, loadSurveysRepositorySpy } = makeSut()
+    const accountId = faker.datatype.uuid()
+    await sut.load(accountId)
+    expect(loadSurveysRepositorySpy.accountId).toBe(accountId)
+  })
+})
