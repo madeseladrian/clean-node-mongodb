445c2b38f6f71d33c24e593e2b5c612f2ae49e2e
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 10:33:19 -0300

    test: ensure LoadSurveysController calls LoadSurveys with correct valueD

---
+++ tests/presentation/controllers/load-surveys-controller.spec.ts
@@ -0,0 +1,38 @@
+import { LoadSurveysController } from '@/presentation/controllers'
+import { LoadSurveysSpy } from '@/tests/presentation/mocks'
+
+import MockDate from 'mockdate'
+import faker from 'faker'
+
+const mockRequest = (): LoadSurveysController.Request => ({ accountId: faker.datatype.uuid() })
+
+type SutTypes = {
+  sut: LoadSurveysController
+  loadSurveysSpy: LoadSurveysSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveysSpy = new LoadSurveysSpy()
+  const sut = new LoadSurveysController(loadSurveysSpy)
+  return {
+    sut,
+    loadSurveysSpy
+  }
+}
+
+describe('LoadSurveys Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('1 - Should call LoadSurveys with correct value', async () => {
+    const { sut, loadSurveysSpy } = makeSut()
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(loadSurveysSpy.accountId).toBe(request.accountId)
+  })
+})
