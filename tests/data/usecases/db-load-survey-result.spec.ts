67d919ca1734d2df6304a242e317e0f79581fe67
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 20:05:04 -0300

    test: DbLoadSuveyResult calls LoadSurveyResultRepository with correct values

---
+++ tests/data/usecases/db-load-survey-result.spec.ts
@@ -0,0 +1,46 @@
+import { DbLoadSurveyResult } from '@/data/usecases'
+import { LoadSurveyResultRepositorySpy, LoadSurveyByIdRepositorySpy } from '@/tests/data/mocks'
+import faker from 'faker'
+import MockDate from 'mockdate'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositorySpy: LoadSurveyResultRepositorySpy
+  loadSurveyByIdRepositorySpy: LoadSurveyByIdRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositorySpy = new LoadSurveyResultRepositorySpy()
+  const loadSurveyByIdRepositorySpy = new LoadSurveyByIdRepositorySpy()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositorySpy, loadSurveyByIdRepositorySpy)
+  return {
+    sut,
+    loadSurveyResultRepositorySpy,
+    loadSurveyByIdRepositorySpy
+  }
+}
+
+let surveyId: string
+let accountId: string
+
+describe('DbLoadSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  beforeEach(() => {
+    surveyId = faker.datatype.uuid()
+    accountId = faker.datatype.uuid()
+  })
+
+  test('1 - Should call LoadSurveyResultRepository with correct values', async () => {
+    const { sut, loadSurveyResultRepositorySpy } = makeSut()
+    await sut.load(surveyId, accountId)
+    expect(loadSurveyResultRepositorySpy.surveyId).toBe(surveyId)
+    expect(loadSurveyResultRepositorySpy.accountId).toBe(accountId)
+  })
+})
