39023427ad048100fc8ebac47f8dee3324b9f444
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 11:14:17 -0300

    test: ensure SurveyMongoRepository load all surveys on success

--- tests/infra/db/mongodb/survey-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/survey-mongo-repository.spec.ts
@@ -1,8 +1,15 @@
 import { MongoHelper, SurveyMongoRepository } from '@/infra/db'
-import { mockAddSurveyParams } from '@/tests/domain/mocks'
-import { Collection } from 'mongodb'
+import { mockAddAccountParams, mockAddSurveyParams } from '@/tests/domain/mocks'
+import { Collection, ObjectId } from 'mongodb'
 
+let accountCollection: Collection
 let surveyCollection: Collection
+let surveyResultCollection: Collection
+
+const mockAccountId = async (): Promise<string> => {
+  const res = await accountCollection.insertOne(mockAddAccountParams())
+  return res.insertedId.toHexString()
+}
 
 const makeSut = (): SurveyMongoRepository => {
   return new SurveyMongoRepository()
@@ -18,8 +25,12 @@ describe('SurveyMongoRepository', () => {
   })
 
   beforeEach(async () => {
-    surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+    surveyCollection = MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    surveyResultCollection = MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
   })
 
   describe('add()', () => {
@@ -28,6 +39,29 @@ describe('SurveyMongoRepository', () => {
       await sut.add(mockAddSurveyParams())
       const count = await surveyCollection.countDocuments()
       expect(count).toBe(1)
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('1 - Should load all surveys on success', async () => {
+      const accountId = await mockAccountId()
+      const addSurveyModels = [mockAddSurveyParams(), mockAddSurveyParams()]
+      const result = await surveyCollection.insertMany(addSurveyModels)
+      const survey = await surveyCollection.findOne({ _id: result.insertedIds[0] })
+      await surveyResultCollection.insertOne({
+        surveyId: survey._id,
+        accountId: new ObjectId(accountId),
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const sut = makeSut()
+      const surveys = await sut.loadAll(accountId)
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].id).toBeTruthy()
+      expect(surveys[0].question).toBe(addSurveyModels[0].question)
+      expect(surveys[0].didAnswer).toBe(true)
+      expect(surveys[1].question).toBe(addSurveyModels[1].question)
+      expect(surveys[1].didAnswer).toBe(false)
     })
   })
 })
