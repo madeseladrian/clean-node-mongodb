fe3270362e775d588dc07fb31529e443434cf2d1
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 10:41:46 -0300

    feat: ensure LoadSurveysController returns 204 if LoadSurveys returns empty

--- src/presentation/controllers/load-surveys-controller.ts
+++ src/presentation/controllers/load-surveys-controller.ts
@@ -1,13 +1,13 @@
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { LoadSurveys } from '@/domain/usecases'
-import { ok } from '@/presentation/helpers'
+import { noContent, ok } from '@/presentation/helpers'
 
 export class LoadSurveysController implements Controller {
   constructor (private readonly loadSurveys: LoadSurveys) {}
 
   async handle (request: LoadSurveysController.Request): Promise<HttpResponse> {
     const surveys = await this.loadSurveys.load(request.accountId)
-    return ok(surveys)
+    return surveys.length ? ok(surveys) : noContent()
   }
 }
 
