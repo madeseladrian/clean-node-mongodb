6c18f7eb14aaee3e7414e7f37e7a73b0bc796306
mades eladrian <mades.eladrian@gmail.com>
2022-07-12 10:39:11 -0300

    test: ensure LoadSurveysController returns 200 on success

--- tests/presentation/controllers/load-surveys-controller.spec.ts
+++ tests/presentation/controllers/load-surveys-controller.spec.ts
@@ -1,6 +1,6 @@
 import { LoadSurveysController } from '@/presentation/controllers'
+import { ok } from '@/presentation/helpers'
 import { LoadSurveysSpy } from '@/tests/presentation/mocks'
-
 import MockDate from 'mockdate'
 import faker from 'faker'
 
@@ -34,5 +34,11 @@ describe('LoadSurveys Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadSurveysSpy.accountId).toBe(request.accountId)
+  })
+
+  test('2 - Should return 200 on success', async () => {
+    const { sut, loadSurveysSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(loadSurveysSpy.result))
   })
 })
