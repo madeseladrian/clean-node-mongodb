066c51aae227190771e5e7f5b734f431ad1d0b46
mades eladrian <mades.eladrian@gmail.com>
2022-07-13 08:59:30 -0300

    ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/usecases/db-load-survey-result.ts
+++ src/data/usecases/db-load-survey-result.ts
@@ -1,29 +1,13 @@
 import { LoadSurveyResult } from '@/domain/usecases'
-import { SurveyModel, SurveyResultModel } from '@/domain/models'
-import { LoadSurveyResultRepository, LoadSurveyByIdRepository } from '@/data/protocols'
+import { LoadSurveyResultRepository } from '@/data/protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (
-    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
-    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
   ) {}
 
   async load (surveyId: string, accountId: string): Promise<LoadSurveyResult.Result> {
     await this.loadSurveyResultRepository.loadBySurveyId(surveyId, accountId)
     return null
-  }
-
-  private makeEmptyResult (survey: SurveyModel): SurveyResultModel {
-    return {
-      surveyId: survey.id,
-      question: survey.question,
-      date: survey.date,
-      answers: survey.answers.map(answer => ({
-        ...answer,
-        count: 0,
-        percent: 0,
-        isCurrentAccountAnswer: false
-      }))
-    }
   }
 }
