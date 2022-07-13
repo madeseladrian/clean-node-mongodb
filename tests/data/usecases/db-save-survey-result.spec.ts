7aa762e447cad15dc9a5355f82792536a83cf20d
mades eladrian <mades.eladrian@gmail.com>
2022-07-13 09:36:51 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

---
+++ tests/data/usecases/db-save-survey-result.spec.ts
@@ -0,0 +1,35 @@
+import { DbSaveSurveyResult } from '@/data/usecases'
+import { mockSaveSurveyResultParams } from '@/tests/domain/mocks'
+import { SaveSurveyResultRepositorySpy } from '@/tests/data/mocks'
+import MockDate from 'mockdate'
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositorySpy: SaveSurveyResultRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositorySpy = new SaveSurveyResultRepositorySpy()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositorySpy)
+  return {
+    saveSurveyResultRepositorySpy,
+    sut
+  }
+}
+
+describe('DbSaveSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('1 - Should call SaveSurveyResultRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositorySpy } = makeSut()
+    const surveyResultData = mockSaveSurveyResultParams()
+    await sut.save(surveyResultData)
+    expect(saveSurveyResultRepositorySpy.params).toEqual(surveyResultData)
+  })
+})
