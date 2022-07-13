ebf9e9ddad67bf2df851827b13c779ec882e60ad
mades eladrian <mades.eladrian@gmail.com>
2022-07-13 09:56:22 -0300

    test: ensure SurveyResulutMongoRespository adds a survey result if its new

---
+++ tests/infra/db/mongodb/survey-result-mongo-repository.spec.ts
@@ -0,0 +1,61 @@
+import { SurveyModel } from '@/domain/models'
+import { SurveyResultMongoRepository, MongoHelper } from '@/infra/db'
+import { mockAddSurveyParams, mockAddAccountParams } from '@/tests/domain/mocks'
+import { Collection, ObjectId } from 'mongodb'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository()
+}
+
+const mockSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne(mockAddSurveyParams())
+  const survey = await surveyCollection.findOne({ _id: res.insertedId })
+  return MongoHelper.map(survey)
+}
+
+const mockAccountId = async (): Promise<string> => {
+  const res = await accountCollection.insertOne(mockAddAccountParams())
+  return res.insertedId.toHexString()
+}
+
+describe('SurveyMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('save()', () => {
+    test('1 - Should add a survey result if its new', async () => {
+      const survey = await mockSurvey()
+      const accountId = await mockAccountId()
+      const sut = makeSut()
+      await sut.save({
+        surveyId: survey.id,
+        accountId,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const surveyResult = await surveyResultCollection.findOne({
+        surveyId: new ObjectId(survey.id),
+        accountId: new ObjectId(accountId)
+      })
+      expect(surveyResult).toBeTruthy()
+    })
+  })
+})
