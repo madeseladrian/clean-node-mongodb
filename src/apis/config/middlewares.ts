d56c08b25e9fb1ecd21c0a9552f6d248391c23bc
mades eladrian <mades.eladrian@gmail.com>
2024-03-07 15:55:36 -0300

    ensure CORS Middleware enables CORS

--- src/apis/config/middlewares.ts
+++ src/apis/config/middlewares.ts
@@ -1,6 +1,7 @@
-import { bodyParser } from '@/apis/middlewares'
+import { bodyParser, cors } from '@/apis/middlewares'
 import { type Express } from 'express'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
