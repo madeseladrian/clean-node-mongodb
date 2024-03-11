453fca04fafd71c3546c8bc52410f6cb147aa158
mades eladrian <mades.eladrian@gmail.com>
2024-03-11 12:54:43 -0300

    ensure adapteRoute defaults req.body and req.params to {} if it is falsy

--- jest.config.js
+++ jest.config.js
@@ -22,7 +22,7 @@ module.exports = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
+    // '!<rootDir>/src/main/**',
      "!**/index.ts"
   ],
 
--- tests/apis/adapters/express-route-adapter.spec.ts
+++ tests/apis/adapters/express-route-adapter.spec.ts
@@ -1,59 +1,125 @@
 import { type Request, type Response } from 'express'
+import { faker } from '@faker-js/faker'
 
 import { adaptRoute } from '@/apis/adapters'
 
 import { type Controller } from '@/application/contracts/controller'
 
-// Função de utilidade para criar um mock do objeto Request do Express
-function createMockRequest (): Partial<Request> {
-  return {
-    body: {},
-    params: {},
-    accountId: 'any_accountId'
-  }
-}
+const mockRequest = (body = {}, params = {}, accountId = ''): Partial<Request> => ({
+  body,
+  params,
+  accountId
+})
 
-// Função de utilidade para criar um mock do objeto Response do Express
-function createMockResponse (): Partial<Response> {
-  const res: Partial<Response> = {}
+const mockResponse = (): Partial<Response> => {
+  const res: any = {}
   res.status = jest.fn().mockReturnThis()
   res.json = jest.fn().mockReturnThis()
   return res
 }
 
 describe('adaptRoute', () => {
-  const controllerMock: jest.Mocked<Controller> = {
-    handle: jest.fn().mockResolvedValueOnce({ statusCode: 200, body: { message: 'any_message' } })
-  }
-  const controllerMockError: jest.Mocked<Controller> = {
-    handle: jest.fn().mockResolvedValueOnce({ statusCode: 400, body: { message: 'any_error' } })
-  }
+  test('Should default req.body to {} if it is falsy', async () => {
+    const value = faker.word.noun()
+    const accountId = faker.string.uuid()
+    const message = faker.lorem.sentence()
 
-  test('Should return a success status and message', async () => {
-    const request = createMockRequest()
-    const response = createMockResponse()
+    const req = mockRequest(null, { param: value }, accountId)
+    const res = mockResponse()
+    const mockController = {
+      handle: jest.fn().mockResolvedValueOnce({ statusCode: 200, body: { message } })
+    }
 
-    const expected = { message: 'any_message' }
-    controllerMock.handle.mockResolvedValueOnce({ statusCode: 200, body: expected })
+    const adapt = adaptRoute(mockController)
+    await adapt(req as Request, res as Response)
 
-    const adapt = adaptRoute(controllerMock)
-    await adapt(request as Request, response as Response)
-
-    expect(response.status).toHaveBeenCalledWith(200)
-    expect(response.json).toHaveBeenCalledWith({ message: 'any_message' })
+    // Expect the controller's handle method to have been called with an object that includes
+    // properties from params and accountId, proving req.body being falsy was defaulted to {}
+    expect(mockController.handle).toHaveBeenCalledWith({
+      param: value,
+      accountId
+    })
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ message })
   })
 
-  test('Should return an error status and message for error responses', async () => {
-    const request = createMockRequest()
-    const response = createMockResponse()
+  test('Should default req.params to {} if it is falsy', async () => {
+    const value = faker.word.noun()
+    const accountId = faker.string.uuid()
+    const message = faker.lorem.sentence()
 
-    const expectedError = { message: 'any_error' }
-    controllerMockError.handle.mockResolvedValueOnce({ statusCode: 400, body: expectedError })
+    const req = mockRequest({ body: value }, null, accountId)
+    const res = mockResponse()
+    const mockController = {
+      handle: jest.fn().mockResolvedValueOnce({ statusCode: 200, body: { message } })
+    }
 
-    const adapt = adaptRoute(controllerMockError)
-    await adapt(request as Request, response as Response)
+    const adapt = adaptRoute(mockController)
+    await adapt(req as Request, res as Response)
+
+    expect(mockController.handle).toHaveBeenCalledWith({
+      body: value,
+      accountId
+    })
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ message })
+  })
+
+  test('Should default req.body and req.params to {} if it is falsy', async () => {
+    const accountId = faker.string.uuid()
+    const message = faker.lorem.sentence()
+    const req = mockRequest(null, null, accountId)
+    const res = mockResponse()
+    const mockController = {
+      handle: jest.fn().mockResolvedValueOnce({ statusCode: 200, body: { message } })
+    }
+
+    const adapt = adaptRoute(mockController)
+    await adapt(req as Request, res as Response)
+
+    expect(mockController.handle).toHaveBeenCalledWith({ accountId })
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({ message })
+  })
+
+  test('should correctly handle a successful response from the controller', async () => {
+    const name = faker.internet.userName()
+    const id = faker.string.uuid()
+    const accountId = faker.string.uuid()
+    const message = faker.lorem.sentence()
+
+    const request = mockRequest({ name }, { id }, accountId)
+    const response = mockResponse()
+
+    const mockSuccessController: Controller = {
+      handle: jest.fn().mockResolvedValueOnce({ statusCode: 200, body: { message } })
+    }
+
+    const adaptedRoute = adaptRoute(mockSuccessController)
+    await adaptedRoute(request as Request, response as Response)
+
+    expect(mockSuccessController.handle).toHaveBeenCalledWith({
+      ...request.body,
+      ...request.params,
+      accountId: request.accountId
+    })
+    expect(response.status).toHaveBeenCalledWith(200)
+    expect(response.json).toHaveBeenCalledWith({ message })
+  })
+
+  test('should correctly handle an error response from the controller', async () => {
+    const errorMessage = faker.lorem.sentence()
+
+    const request = mockRequest()
+    const response = mockResponse()
+    const mockErrorController: Controller = {
+      handle: jest.fn().mockResolvedValueOnce({ statusCode: 400, body: { message: errorMessage } })
+    }
+
+    const adaptedRoute = adaptRoute(mockErrorController)
+    await adaptedRoute(request as Request, response as Response)
 
     expect(response.status).toHaveBeenCalledWith(400)
-    expect(response.json).toHaveBeenCalledWith({ error: expectedError.message })
+    expect(response.json).toHaveBeenCalledWith({ error: errorMessage })
   })
 })
